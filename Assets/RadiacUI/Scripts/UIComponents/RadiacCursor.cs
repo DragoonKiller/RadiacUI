26fe9ec9f766591f0357b07f0a0d263e996f75a8
DragoonKiller <947426443@qq.com>
2018-06-18 13:39:03 +0800

    update cursor.

--- Assets/RadiacUI/Scripts/UIComponents/RadiacCursor.cs
+++ Assets/RadiacUI/Scripts/UIComponents/RadiacCursor.cs
@@ -12,22 +12,21 @@ namespace RadiacUI
         protected virtual void Start()
         {
             // Cursor.lockState = CursorLockMode.Confined;
-            // Cursor.visible = false;
-            
+            Cursor.visible = false;
             QualitySettings.vSyncCount = 0;
         }
         
         protected virtual void OnDestroy()
         {
-            Cursor.lockState = CursorLockMode.None;
-            // Cursor.visible = true;
+            // Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
         }
         
         protected virtual void Update()
         {
-            // The position sync is direct snyc.
-            // Turn off the V-Blank sync will recduce the latency to exact one frame.
-            // Or the latency will be obvious...
+            // There *might* be an obvious lag of cursor.
+            // Obvious in Editor but not obvious in build if hide the cursor.
+            // Keep it stay. Keep it good.
             this.gameObject.transform.position = VirtualCursor.position;
         }
         
