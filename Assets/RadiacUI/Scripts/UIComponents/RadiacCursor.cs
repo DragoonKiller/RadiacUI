5cd6d9b33452d51edb68388ab6bdcb670a1904ab
DragoonKiller <947426443@qq.com>
2018-07-04 19:02:32 +0800

    update to incremental compiler. add cursor.lock.

--- Assets/RadiacUI/Scripts/UIComponents/RadiacCursor.cs
+++ Assets/RadiacUI/Scripts/UIComponents/RadiacCursor.cs
@@ -2,6 +2,8 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 
+using System.Runtime.InteropServices;
+
 namespace RadiacUI
 {
     /// <summary>
@@ -16,31 +18,78 @@ namespace RadiacUI
             inst = this;
         }
         
+        // Used by virtual cursor.
         public float speedMult = 15.0f;
         
-        protected virtual void Start()
+        // ============================================================================================================
+        // Windows Specific Section
+        // ============================================================================================================
+        
+        #if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+        
+        [DllImport("user32")]
+        static extern int SetCursorPos(int x, int y);
+        
+        [DllImport("user32")]
+        static extern IntPtr GetActiveWindow();
+        
+        [DllImport("user32")]
+        static extern bool GetWindowRect(IntPtr window, IntPtr rect);
+        
+        [StructLayout(LayoutKind.Explicit)]
+        struct WinRect
         {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-            QualitySettings.vSyncCount = 0;
+            [FieldOffset(0x00)] public int left;
+            [FieldOffset(0x04)] public int top;
+            [FieldOffset(0x08)] public int right;
+            [FieldOffset(0x0C)] public int bottom;
         }
         
-        protected virtual void OnDestroy()
+        bool focused = false;
+        
+        void Start()
         {
-            Cursor.lockState = CursorLockMode.None;
+            focused = true;
+        }
+        
+        void OnDestroy()
+        {
             Cursor.visible = true;
         }
         
-        protected virtual void Update()
+        void Update()
         {
+            Cursor.visible = false;
+            
+            if(Input.GetKey(KeyCode.Mouse0))
+            {
+                focused = true;
+            }
+            
+            if(focused)
+            {
+                SetCursorPos(0, 0);
+                unsafe
+                {
+                    WinRect rect = new WinRect();
+                    var window = GetActiveWindow();
+                    GetWindowRect(window, new IntPtr(&rect));
+                    SetCursorPos((rect.left + rect.right) / 2, (rect.top + rect.bottom) / 2);
+                }
+            }
+            
             // There *might* be an obvious lag of cursor.
             // Obvious in Editor but not obvious in build if hide the cursor.
             // Keep it stay. Keep it good.
             this.gameObject.transform.position = VirtualCursor.position;
         }
         
-        
-        
+        void OnApplicationFocus(bool isFocus)
+        {
+            focused = isFocus;
+        }
+    
+        #endif // UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
     }
     
 }
